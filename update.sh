c97d46ad79fae95bea1dc42587808028023956e0
wildonion <ea_pain@yahoo.com>
2020-01-22 10:55:48 +0330

    add update script for updating the vps

---
+++ update.sh
@@ -0,0 +1,7 @@
+
+
+
+ # 1) JEKYLL_ENV=production bundle exec jekyll build
+ # 2) push _site to uniXerr-blog-jekyll_static repo
+ # 3) ogin to vps and pull the repo on /var/www/unixerr.com/
+ # 4) sudo systemctl nginx restart
